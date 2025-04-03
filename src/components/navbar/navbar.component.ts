11401a1ec43ded9ca64e0a12b26f6127e1305844
Jhon Harold Ariza Suaza <jariza@heinsohn.com.co>
2025-04-03 18:23:51 -0500

    ajuste menu

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  closeOffcanvas(event: Event) {
+    const el = event.target as HTMLElement;
+  
+    // Solo cerramos si el click fue en un <a>
+    if (el.tagName.toLowerCase() === 'a') {
+      const offcanvasEl = document.getElementById('sidebarResponsive');
+      if (offcanvasEl) {
+        const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasEl);
+        if (bsOffcanvas) {
+          bsOffcanvas.hide();
+        }
+      }
+    }
+  }
 }
